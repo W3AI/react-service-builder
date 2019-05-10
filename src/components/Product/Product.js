ca594b6b2ce84a083ec277b37b2e7acd6379dcb6
stefian <v_ianta@yahoo.com>
2019-05-10 15:49:53 -0400

    calculate the ingredient sum dynamically

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,12 +4,19 @@ import classes from './Product.module.css';
 import ProductIngredient from './ProductIngredient/ProductIngredient';
 
 const product = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <ProductIngredient key={igKey + i} type={igKey} />
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+    // console.log(transformedIngredients);
     return (
         <div className={classes.Product}>
             <ProductIngredient type="bread-top" />
