3a27ce3a26d440db09a81ff5906e8883e8c567ea
stefian <v_ianta@yahoo.com>
2019-05-10 20:50:16 -0400

    added logic to enable or disable purchase on total items > 0 true

--- src/containers/ProductBuilder/ProductBuilder.js
+++ src/containers/ProductBuilder/ProductBuilder.js
@@ -23,7 +23,22 @@ class ProductBuilder extends Component {
             cheese: 0,
             meat: 0,
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePurchaseState () {
+        const ingredients = {
+            ...this.state.ingredients
+        };
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey]
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        this.setState({purchasable: sum > 0})
     }
 
     addIngredientHandler = (type) => {
@@ -70,6 +85,7 @@ class ProductBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler} 
                     disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
                     price={this.state.totalPrice} />
             </Aux>
         );
