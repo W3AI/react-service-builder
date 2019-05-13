e2183788ec45c67e4f415a56c47942e706f793ad
stefian <v_ianta@yahoo.com>
2019-05-13 19:52:31 -0400

    added responsive adjustments for the Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import productLogo from '../../assets/images/M8-logo.png';
 import classes from './Logo.module.css';
 
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={productLogo} alt="M8 sMart Engine" />
     </div>
 );
